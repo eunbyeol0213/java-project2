4a3fcf0f4bf31cd0b2c62c0a4f2dfa9aa98c1299
KoEunByeol <aygodanwn@gmail.com>
2023-04-20 15:01:23 +0900

    feat: 변수, 타입 - 평균구하기 - 변수 선언

---
+++ src/main/java/com/example/javaproject2/week1/day4/ByteTypeExercise.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week1.day4;
+
+public class ByteTypeExercise {
+    public static void main(String[] args) {
+        byte byte1 = 10;
+        byte byte2 = -127;
+        System.out.println(byte1);
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week1/day4/FloatFormatEx.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week1.day4;
+
+public class FloatFormatEx {
+    public static void main(String[] args) {
+        System.out.printf("%f\n", 0.5);
+        System.out.printf("%.1f\n", 0.5);
+        System.out.printf("%.2f\n", 0.5);
+
+    }
+}
