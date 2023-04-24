2c7d624531e85e0f352b9ba8f52838222cfe91a6
KoEunByeol <aygodanwn@gmail.com>
2023-04-24 15:41:45 +0900

    feat: float, double ,CodeUp 1021~1023, CodeUp 1028~1030

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1021.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1021 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String data = sc.next();
+
+        System.out.println(data);
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week2/DefineAVariable.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week2;
+
+public class DefineAVariable {
+    int num = 10;
+    String name = "고은별";
+
+    String sOne = new String("1");
+    int iOne = 1;
+    float fOne = 1.0f;
+}
